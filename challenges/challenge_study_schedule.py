c3fc3cd00ae0ddc1af59c232c6be3fce37bd144d
Amanda Portela <mandaptela@gmail.com>
2023-07-07 22:26:59 -0300

    Requisito 1 - Study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    times = 0
+    if type(target_time) != int:
+        return None
+    for login, logout in permanence_period:
+        if type(login) != int or type(logout) != int:
+            return None
+        if target_time >= login and target_time <= logout:
+            times += 1
+    return times
+# study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5)
+
